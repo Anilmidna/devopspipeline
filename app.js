130d98169e4bb18a5ec3f649e8d7f01175bc72c9
Anilmidna <anilmidna@gmail.com>
2026-02-28 20:43:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
       </head>
       <body>
         <div class="box">
-          <h1>🎉 THIS IS FIRST WEBSITE!</h1>
+          <h1>🎉 THIS IS SECOND CHANGE TO TEST AUTOMATED PIPELINE!</h1>
           <p>Your new website has been successfully deployed.</p>
           <p class="highlight">Hosted on AWS Elastic Beanstalk 🚀</p>
           <p>CI powered by AWS CodeBuild</p>
